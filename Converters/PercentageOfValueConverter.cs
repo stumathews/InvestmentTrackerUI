a42b0e2871e160ede6e45232249d8581c9be42a2
Stuart Mathews <stumathews@gmail.com>
2021-05-31 23:59:42 +0100

    Use PercentageOfValue converter

---
+++ Converters/PercentageOfValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InvestmentTrackerUI.Converters
+{
+     [ValueConversion(typeof(double), typeof(double))]
+    public class PercentageOfValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (double)value;
+            var percentageOf = double.Parse((string)parameter);
+            return val * percentageOf;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
